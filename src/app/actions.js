f3a0f134a8fbb79fb6d378ea82304d86a84a8fa7
walid1508 <2703593@collegelacite.ca>
2025-08-09 10:32:29 -0400

    Permettre aux utilisateurs d'envoyer des avis sur les restaurants

--- src/app/actions.js
+++ src/app/actions.js
@@ -7,4 +7,18 @@ import { getFirestore } from "firebase/firestore";
 // This is a Server Action
 // https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions
 // Replace the function below
-export async function handleReviewFormSubmission(data) {}
+// This is a next.js server action, which is an alpha feature, so
+// use with caution.
+// https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions
+export async function handleReviewFormSubmission(data) {
+  const { app } = await getAuthenticatedAppForUser();
+  const db = getFirestore(app);
+
+  await addReviewToRestaurant(db, data.get("restaurantId"), {
+    text: data.get("text"),
+    rating: data.get("rating"),
+
+    // This came from a hidden form field.
+    userId: data.get("userId"),
+  });
+}
